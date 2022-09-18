c628560c2c301ad0a9a428eb83481e6c64cda6d9
harshjainsk <harshjainsk531@gmail.com>
2022-09-18 15:05:46 +0530

    created basic layout for app

--- faceid.py
+++ faceid.py
@@ -11,12 +11,10 @@ from kivy.uix.image import Image  # used for real-time webcam feed
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 
-
 # """used for other functionalities"""
 from kivy.clock import Clock  # used for continous real-time swap
 from kivy.graphics.texture import Texture
 from kivy.logger import Logger
-
 
 # import other dependencies
 import tensorflow as tf
@@ -24,3 +22,26 @@ import cv2
 import numpy as np
 import os
 from layers import L1Dist
+
+"""Build app and layout"""
+
+
+class CamApp(App):
+
+    def build(self):
+        # Main layout components
+        self.img1 = Image(size_hint=(1, 0.8))
+        self.button = Button(text='Verify', size_hint=(1, 0.1))
+        self.verification = Label(text='Verification uninitalised', size_hint=(1, 0.1))
+
+        # Add items to layout
+        layout = BoxLayout(orientation = 'vertical')
+        layout.add_widget(self.img1)
+        layout.add_widget(self.button)
+        layout.add_widget(self.verification)
+
+        return layout
+
+
+if __name__ == '__main__':
+    CamApp().run()
