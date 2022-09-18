95c05ce99a2f7a36c1d3da7cf84d67cdf75f96b1
harshjainsk <harshjainsk531@gmail.com>
2022-09-18 16:03:54 +0530

    added verification function

--- faceid.py
+++ faceid.py
@@ -76,9 +76,34 @@ class CamApp(App):
         img = tf.io.decode_jpeg(byte_image)  # loading of image
         img = tf.image.resize(img, (100, 100))
         img = img / 255.0
-
-    #
         return img
+
+    # Adding verify function
+    def verify(model, detection_threshold, verification_threshold):
+        """
+            detection_threshold means the value above which a prediction is considered positive
+            verification_threshold is proportion of positive predictions by total positive samples
+        """
+
+        # Build results array
+        results = []
+
+        for image in os.listdir(os.path.join('application_data', 'verification_images')):
+            input_img = preprocess(os.path.join('application_data', 'input_image', 'input_image.jpg'))
+            validation_img = preprocess(os.path.join('application_data', 'verification_images', image))
+
+            # Make Predictions
+            result = model.predict(list(np.expand_dims([input_img, validation_img], axis=1)))
+            results.append(result)
+
+        # Detection Threshold: Metric above which a prediction is considered positive
+        detection = np.sum(np.array(results) > detection_threshold)
+
+        # Verification Threshold: Proportion of positive predictions / total positive samples
+        verification = detection / len(os.listdir(os.path.join('application_data', 'verification_images')))
+        verified = verification > verification_threshold
+
+        return results, verified
 
 
 if __name__ == '__main__':
